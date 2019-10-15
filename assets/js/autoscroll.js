6af3414241ecd0e62427a4b3800235764832750e
George Hess <george.hess@reelworld.com>
2019-10-15 15:16:38 -0700

    auto scroll on next frame

--- assets/js/autoscroll.js
+++ assets/js/autoscroll.js
@@ -1,7 +1,13 @@
 if (!!location.hash && document.referrer.startsWith(location.protocol + "//" + location.host)) {
+	var scrollToElement;
 	if (location.hash === "#read-more") {
-		document.querySelector(".story-cite").scrollIntoView({behavior: "smooth"});
+		scrollToElement = document.querySelector(".story-cite");
 	} else if (location.hash.startsWith("#list-")) {
-		document.getElementById(location.hash.slice(6)).scrollIntoView({behavior: "smooth"});
+		scrollToElement = document.getElementById(location.hash.slice(6));
+	}
+	if (scrollToElement) {
+		requestAnimationFrame(function() {
+			scrollToElement.scrollIntoView({behavior: "smooth"});
+		});
 	}
 }
